4779ffc60a8386c6c62c686dd23e34a21affc2a5
Federico Feroldi <fferoldi@measurence.com>
2016-12-28 18:01:55 +0100

    Working video play (ex2)

--- Makefile
+++ Makefile
@@ -4,9 +4,12 @@ SRCS=$(wildcard src/*.cpp)
 
 PROGS=$(patsubst src/%.cpp,bin/%,$(SRCS))
 
-CFLAGS=$(shell pkg-config --cflags --libs /usr/local/Cellar/opencv3/HEAD-7dd3723_4/lib/pkgconfig/opencv.pc)
+CFLAGS=$(shell pkg-config --cflags --libs /usr/local/lib/pkgconfig/opencv.pc)
 
 all: $(PROGS)
+
+clean:
+	rm $(PROGS)
 
 bin/%: src/%.cpp
 
--- src/ex2.cpp
+++ src/ex2.cpp
@@ -1,18 +1,41 @@
-#include <opencv2/highgui/highgui.hpp>
+#include <string>
+#include <iostream>
 
-using namespace cv;
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
-int main(int argc, char **argv) {
+int main( int argc, char **argv ) {
 
-  Mat img = imread(argv[1], -1);
-  if(img.empty()) {
-    return -1;
+  cv::namedWindow("Example3", cv::WINDOW_AUTOSIZE);
+
+  cv::VideoCapture cap;
+  auto filename = std::string(argv[1]);
+
+  std::cout << "Opening [" << filename << "]\n";
+
+  cap.open(filename);
+
+  cv::Mat frame;
+
+  std::cout << "Starting loop\n";
+
+  while(cap.isOpened()) {
+    cap >> frame;
+
+    // std::cout << frame.elemSize() << "\n";
+
+    if(frame.empty()) {
+      std::cout << "Empty frame\n";
+      break;
+    }
+    cv::imshow("Example3", frame);
+    if((cv::waitKey(33) & 0xff) == 'q') {
+      std::cout << "Key pressed\n";
+      break;
+    }
   }
 
-  namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+  std::cout << "Ending loop\n";
 
-  imshow("Example1", img);
-  waitKey(0);
-
-  destroyWindow("Example1");
+  return 0;
 }
